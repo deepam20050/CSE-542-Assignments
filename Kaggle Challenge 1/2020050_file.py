871c6a3015fb48ddba565f65057c7b5b7ac76515
Deepam Sarmah <deepam20050@iiitd.ac.in>
2023-03-24 22:30:04 +0530

    Kaggle Challenge 1

--- Kaggle Challenge 1/2020050_file.py
+++ Kaggle Challenge 1/2020050_file.py
@@ -1,47 +1,26 @@
+'''
+  Deepam Sarmah
+  200050
+  deepam20050@iiitd.ac.in
+'''
+
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import BaggingClassifier
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn import svm
-from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+import joblib
 
+train_df = pd.read_csv("Train.csv")
+test_df = pd.read_csv("Test.csv")
 
-train_df = pd.read_csv('Train.csv')
-test_df = pd.read_csv('Test.csv')
+train_features = train_df.drop(["Id", "target"], axis=1)
+train_labels = train_df["target"]
 
-X_train = train_df.drop('target', axis=1)
-y_train = train_df['target']
+trainX, valX, trainY, valY = train_test_split(train_features, train_labels, test_size=0.33, random_state=42)
 
-X_train.drop('Id', inplace=True, axis=1)
+lgreg = LogisticRegression(random_state=0)
+lgreg.fit(trainX, trainY)
+predY = lgreg.predict(test_df.drop(["Id"], axis=1))
 
-X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
-
-scaler = StandardScaler().fit(X_train)
-X_train = scaler.transform(X_train)
-
-# dtc = DecisionTreeClassifier()
-
-# bagging = BaggingClassifier(estimator=dtc, n_estimators=1000, max_samples=150)
-
-# bagging.fit(X_train, y_train)
-
-# gb_clf = GradientBoostingClassifier(loss="exponential", n_estimators=100000, learning_rate=1.95, max_features=10, max_depth=20, random_state=0)
-# gb_clf.fit(X_train, y_train)
-
-# nb = GaussianNB()
-# nb.fit(X_train, y_train)
-
-# logreg = LogisticRegression(tol=1e-12, solver="liblinear", max_iter=1000000)
-# logreg.fit(X_train, y_train)
-
-clf = svm.SVC(kernel='linear', max_iter=-1) # Linear Kernel
-clf.fit(X_train, y_train)
-
-
-y_pred = clf.predict(scaler.transform(test_df.drop("Id", axis=1)))
-
-predictions = pd.DataFrame({'Id': test_df.Id, 'target': y_pred})
+predictions = pd.DataFrame({'Id': test_df.Id, 'target': predY})
 predictions.to_csv("submission.csv", index=False)
+joblib.dump(lgreg, "2020050_model.pkl")
